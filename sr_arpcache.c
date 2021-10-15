badf6d66752244f46b1fcffd055abc45c35bd19e
ChloeZ1208 <dongqingzhu1208@hotmail.com>
2021-10-15 16:15:02 -0400

    fix:ping server segmentation fault-arp req

--- sr_arpcache.c
+++ sr_arpcache.c
@@ -29,6 +29,7 @@ void sr_arpcache_sweepreqs(struct sr_instance *sr) {
 void handle_arpreq(struct sr_arpreq * request, struct sr_instance *sr) {
     time_t now = time(0);
     if (difftime(now, request->sent) >= 1.0) {
+        printf("in handle arp?\n");
         if(request->times_sent >= 5) {
             struct sr_packet *packets = request->packets;
             while (packets) {
